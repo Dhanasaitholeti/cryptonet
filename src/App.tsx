60c72fc5a5165c04a88d0ae14e343c5c1ea59552
Dhanasai Tholeti <dhanasai5566@gmail.com>
2024-02-11 07:38:25 +0530

    added initial code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen w-full flex align-middle justify-center">
+      <h1 className="text-4xl text-red-600">This is Home Page</h1>
     </div>
   );
-}
+};
 
 export default App;
